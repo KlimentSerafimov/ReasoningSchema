df1d747482b5718a6adbb4f2b6867b4387e03614
Kliment Serafimov <kliment@mit.edu>
2020-04-09 14:03:11 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/CompactPoset.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompactPoset.dir/main.cpp.o"
+  "CompactPoset"
+  "CompactPoset.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CompactPoset.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
